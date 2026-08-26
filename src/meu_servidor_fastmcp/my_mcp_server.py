f4e220495eee257759d17e11dc067794ce437b90
Cristiano Da Silva Muroni <cristiano.muroni@semantix.ai>
2026-08-25 23:42:25 -0300

    to check user´s CPF

--- src/meu_servidor_fastmcp/my_mcp_server.py
+++ src/meu_servidor_fastmcp/my_mcp_server.py
@@ -1,5 +1,6 @@
 from fastmcp import FastMCP
 import requests
+import re
 
 mcp = FastMCP("My MCP Server")
 
@@ -20,6 +21,76 @@ def get_client(codigo: int) -> dict:
         "codigo": codigo,
         "nome": "Ford",
         "cidade": "Mogi-Guaçu"
+    }
+
+@mcp.tool
+def to_check_cpf(texto: str) -> dict:
+    """
+    Extrai e valida um CPF a partir de qualquer texto informado.
+    """
+
+    cpf = re.sub(r"\D", "", texto)
+
+    cpf = cpf[:11]
+
+    if len(cpf) != 11:
+        return {
+            "valid": False,
+            "cpf": None
+        }
+
+    if cpf in {
+        "00000000000",
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999",
+    }:
+        return {
+            "valid": False,
+            "cpf": cpf
+        }
+
+    soma = 0
+
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+
+    resto = (soma * 10) % 11
+
+    if resto in (10, 11):
+        resto = 0
+
+    if resto != int(cpf[9]):
+        return {
+            "valid": False,
+            "cpf": cpf
+        }
+
+    soma = 0
+
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+
+    resto = (soma * 10) % 11
+
+    if resto in (10, 11):
+        resto = 0
+
+    if resto != int(cpf[10]):
+        return {
+            "valid": False,
+            "cpf": cpf
+        }
+
+    return {
+        "valid": True,
+        "cpf": cpf
     }
 
 
