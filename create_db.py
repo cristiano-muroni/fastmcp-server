5258cb23e1c65d736218eda6a5ed6bc8a62a317b
Cristiano Da Silva Muroni <cristiano.muroni@semantix.ai>
2026-09-03 00:05:07 -0300

    coment create.db

--- create_db.py
+++ create_db.py
@@ -1,38 +1,37 @@
-# create_db.py
-import sqlite3
+# import sqlite3
 
-conn = sqlite3.connect("src/database/beneficiarios.db")
+# conn = sqlite3.connect("src/database/beneficiarios.db")
 
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS beneficiarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    matricula TEXT NOT NULL,
-    cpf TEXT NOT NULL,
-    beneficiario TEXT NOT NULL,
-    plano TEXT NOT NULL
-)
-""")
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS beneficiarios (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     matricula TEXT NOT NULL,
+#     cpf TEXT NOT NULL,
+#     beneficiario TEXT NOT NULL,
+#     plano TEXT NOT NULL
+# )
+# """)
 
-cursor.executemany(
-    """
-INSERT INTO beneficiarios (
-    nome,
-    matricula,
-    cpf,
-    beneficiario,
-    plano
-)
-VALUES (?, ?, ?, ?, ?)
-""",
-    [
-        ("João Silva", "1001", "12345678901", "Titular", "Premium"),
-        ("Maria Silva", "1002", "98765432100", "Dependente", "Premium"),
-        ("Carlos Souza", "1003", "11122233344", "Titular", "Empresarial"),
-    ],
-)
+# cursor.executemany(
+#     """
+# INSERT INTO beneficiarios (
+#     nome,
+#     matricula,
+#     cpf,
+#     beneficiario,
+#     plano
+# )
+# VALUES (?, ?, ?, ?, ?)
+# """,
+#     [
+#         ("João Silva", "1001", "12345678901", "Titular", "Premium"),
+#         ("Maria Silva", "1002", "98765432100", "Dependente", "Premium"),
+#         ("Carlos Souza", "1003", "11122233344", "Titular", "Empresarial"),
+#     ],
+# )
 
-conn.commit()
-conn.close()
+# conn.commit()
+# conn.close()
